7e8313a77bad1d5e76920d8c1e81e122666ba38a
Ivy Chang <ivycwq@icloud.com>
2016-11-02 21:13:29 -0700

    final classes

--- AppUpdaterTest.java
+++ AppUpdaterTest.java
@@ -1,12 +1,14 @@
+import java.util.ArrayList;
+
 public class AppUpdaterTest {
   public static void main(String[] args) {
-    AppUpdater myAppUpdater = new AppUpdater();
+    AppUpdater myAppUpdater = AppUpdater.getInstance();
 
     SpotifyApp spotify = new SpotifyApp();
     ChromeApp chrome = new ChromeApp();
     SkypeApp skype = new SkypeApp();
 
-    myAppUpdater.registerProgram(new ArrayList<Command> {{add(spotify::updateSpotify), add(chrome::updateChrome), add(skype::updateSkype)}});
+    myAppUpdater.registerProgram(new ArrayList<Command>() {{add(spotify::updateSpotify); add(chrome::updateChrome); add(skype::updateSkype);}});
 
     myAppUpdater.scheduledTimeIsUp();
   }
